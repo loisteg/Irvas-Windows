36a8fa60cbb1c118c8b77e115c34dfe19bd7ddf8
Loisteg <loisteg@outlook.com>
2022-06-07 01:04:05 +0300

    Working w/ forms on the website

---
+++ src/js/modules/forms.js
@@ -0,0 +1,56 @@
+const forms = () => {
+  const form = document.querySelectorAll("form"),
+    inputs = document.querySelectorAll("input"),
+    phoneInputs = document.querySelectorAll("input[name='user_phone']");
+
+  phoneInputs.forEach((input) => {
+    input.addEventListener("input", () => {
+      input.value = input.value.replace(/\D/, "");
+    });
+  });
+
+  const message = {
+    loading: "Загрузка...",
+    success: "Спасибо! Скоро с Вами свяжуться",
+    failure: "Что-то пошло не так...",
+  };
+
+  const postData = async (url, data) => {
+    document.querySelector(".status").textContent = message.loading;
+    let res = await fetch(url, {
+      method: "POST",
+      body: data,
+    });
+    return await res.text();
+  };
+
+  const clearInputs = () => {
+    inputs.forEach((input) => (input.value = ""));
+  };
+
+  form.forEach((item) => {
+    item.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      let statusMessage = document.createElement("div");
+      statusMessage.classList.add("status");
+      item.appendChild(statusMessage);
+
+      const formData = new FormData(item);
+      postData("assets/server.php", formData)
+        .then((res) => {
+          console.log(res);
+          statusMessage.textContent = message.success;
+        })
+        .catch(() => (statusMessage.textContent = message.failure))
+        .finally(() => {
+          clearInputs();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 3000);
+        });
+    });
+  });
+};
+
+export default forms;
